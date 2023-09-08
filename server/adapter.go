44be11b3d275baceb01e7d8a34b15db8bc6125ed
zql <1241236275@qq.com>
2023-09-08 16:30:35 +0800

    update adapter

--- server/adapter.go
+++ server/adapter.go
@@ -3,11 +3,16 @@ package server
 import (
 	"CodeArena/conf"
 	"fmt"
-	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+	"go.uber.org/zap"
+	"xorm.io/xorm"
+	"xorm.io/xorm/names"
 )
 
+var engine *xorm.Engine
+
 func init() {
-	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8",
 		conf.V.GetString("mysql.user"),
 		conf.V.GetString("mysql.pwd"),
 		conf.V.GetString("mysql.host"),
@@ -15,5 +20,14 @@ func init() {
 		conf.V.GetString("mysql.db"),
 	)
 
-	sqlx.NewMysql(dns)
+	var err error
+
+	engine, err = xorm.NewEngine("mysql", dns)
+
+	if err != nil {
+		zap.L().Error(fmt.Sprintf("数据库连接异常 %v", err))
+	}
+
+	engine.ShowSQL(true)
+	engine.SetMapper(names.GonicMapper{})
 }
